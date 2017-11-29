e668281addf2be9dfd07348d8fa9856e4ebd1b4a
joaotostes <joao_tostes17@hotmail.com>
2017-11-29 13:43:44 -0200

    Adicionando arquivos

---
+++ crud.js
@@ -0,0 +1,58 @@
+$("#enviar").on("click", function () {
+    var nome = $("#txtNome").val()
+    $("#tabelaNomes").append("<tr/><td>" + nome + "<td>")
+    // Adicionando no localStorage
+    localStorage.setItem('Nome', nome)
+    $("#txtNome").val("");
+})
+$("#fechaGridCadas").on("click", function () {
+    $("#fieldNovoCadastro").slideUp(200)
+
+})
+$("#fechaGridEdicao").on("click", function () {
+    $("#fieldEditaCadastro").slideUp(200)
+})
+
+
+$("#novoCadas").click(function () {
+    $("#fieldNovoCadastro").slideDown(200)
+});
+
+$("#editaCadas").click(function () {
+    $("#fieldEditaCadastro").slideDown(200)
+})
+
+
+$("#editaCadas").on("click", function () {
+    localStorage.removeItem('Nome');
+    var nomeAtualizado = $("#attNome").val();
+    var local = localStorage.setItem('NomeAtualizado', nomeAtualizado)
+})
+
+$("#deleta").on("click", function () {
+    localStorage.removeItem('NomeAtualizado')
+})
+
+$("#cpf").keyup(function () {
+    mcpf($("#cpf").val());
+}).keydown(function (e) {
+    if (e.which == 109 || e.which == 107 || e.which == 69 || e.which == 189 || e.which == 188 || e.which == 190)
+        return false;
+    mcpf($("#cpf").val());
+}).on("blur", function () {
+    mcpf($("#cpf").val());
+});
+
+function mcpf(v) {
+    //retirando caracteres a mais do campo
+    if ($("#cpf").val().length > 14) {
+        $("#cpf").val($("#cpf").val().substr(0, 13));
+        $("#cpf").keydown();
+    }
+
+    v = v.replace(/\D/g, "");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d)/, "$1.$2");
+    v = v.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    $("#cpf").val(v);
+}
